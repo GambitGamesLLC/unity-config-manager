c6f5a0d3c363a59397072618f53761cf6e517923
Derrick Barra <gambitgamesllc@gmail.com>
2025-06-20 16:19:31 -0400

    Initial upload

---
+++ Assets/Plugins/Package/Runtime/Scripts/ConfigManager.cs
@@ -0,0 +1,67 @@
+﻿/********************************************************
+ * ConfigManager.cs
+ * 
+ * Reads and writes a json config file
+ * 
+ ********************************************************/
+
+
+#region IMPORTS
+
+#if GAMBIT_SINGLETON
+using gambit.singleton;
+#else
+/// <summary>
+/// Fallback Singleton base class if GAMBIT_SINGLETON is not defined.
+/// </summary>
+/// <typeparam name="T">Type of the MonoBehaviour singleton.</typeparam>
+public class Singleton<T>: MonoBehaviour where T : MonoBehaviour
+{
+    private static T instance;
+
+    /// <summary>
+    /// Gets the singleton instance, creating it if necessary.
+    /// </summary>
+    //---------------------------------------------//
+    public static T Instance
+    //---------------------------------------------//
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameObject( typeof( T ).Name ).AddComponent<T>();
+                GameObject.DontDestroyOnLoad( instance.gameObject );
+            }
+            return instance;
+        }
+    }
+
+} //END Singleton<T> class
+#endif
+
+using System;
+using UnityEngine;
+
+#endregion
+
+namespace gambit.config
+{
+
+    /// <summary>
+    /// Singleton Manager for reading and writing json config files
+    /// </summary>
+    public class ConfigManager : Singleton<ConfigManager>
+    {
+        #region PUBLIC - VARIABLES
+
+        /// <summary>
+        /// Current instance of the ConfigSystem instantiated during Create()
+        /// </summary>
+        //public static ConfigSystem system;
+
+        #endregion
+
+    } //END ConfigManager Class
+
+} //END gambit.config Namespace
